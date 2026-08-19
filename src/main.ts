1d957b99d7a0a0fda6e0e3fdda77d672d2820d6b
dsad233 <ksl1421@naver.com>
2026-08-19 17:46:04 +0900

    fix: main.ts 수정

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,45 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
+import express, { Express } from 'express';
+import GlobalsRotuer from './globals/globals.router';
 import AuthRouter from './auth/auth.router';
 import UsersRouter from './users/users.router';
 import PostsRouter from './posts/posts.router';
 import CommentsRouter from './comments/comments.router';
 import LikesRouter from './likes/likes.router';
+import CategoryRouter from './categories/categories.router';
 
 import ErrorMiddleware from './common/middlewares/errorMiddleware';
-
-import { HttpError } from 'http-errors';
-import { prisma } from './common/configs/prisma-client';
-import { redisCondition } from './redis/redis.config';
-import cookieParser from 'cookie-parser';
-import Cors from './common/middlewares/core';
 import { MorganMiddleware } from './common/middlewares/morgan.middlewares';
+import Cors from './common/middlewares/cors';
+
+import { prisma } from './common/configs/prisma-client';
+import cookieParser from 'cookie-parser';
+import { MongoDBConfig } from './common/configs/mongodb.config';
+import passport from 'passport';
+import { GoogleStrategy } from './common/middlewares/googleStrategy';
 
 const app: Express = express();
 const port: number = 3000;
 
-app.use(express.json());
+// req.body 수용 용량을 기존 100kb에서 50mb 수용으로 변경
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(Cors());
 
-// Redis 상태 체크
-redisCondition();
-
+MongoDBConfig();
+// passport 설정
+passport.use(GoogleStrategy());
+// morgan logger 설정
 app.use(MorganMiddleware());
 
 app.use('/auth', AuthRouter);
 app.use('/users', UsersRouter);
-app.use('/posts', PostsRouter);
-app.use('/posts', CommentsRouter);
-app.use('/posts', LikesRouter);
+app.use('/posts', [PostsRouter, CommentsRouter, LikesRouter]);
+app.use('/globals', GlobalsRotuer);
+app.use('/categories', CategoryRouter);
 
-app.use((error: HttpError, req: Request, res: Response, next: NextFunction) =>
-  ErrorMiddleware(error, req, res, next),
-);
+// 에러 핸들링 미들웨어
+app.use(ErrorMiddleware);
 
 app.listen(port, () => {
   prisma.$connect();
@@ -49,6 +53,6 @@ process.on('exit', () => {
   setTimeout(() => {
     prisma.$disconnect();
 
-    process.exit();
+    process.exit(1);
   }, 2000);
 });
