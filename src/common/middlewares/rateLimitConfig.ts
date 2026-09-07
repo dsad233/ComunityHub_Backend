6f26f7dc8312c88558dcba9c2ad8b458a0edcfd2
dsad233 <ksl1421@naver.com>
2026-09-07 23:16:28 +0900

    fix: ratelimit 설정 변경

--- src/common/middlewares/rateLimitConfig.ts
+++ src/common/middlewares/rateLimitConfig.ts
@@ -1,17 +1,31 @@
-import { Request } from 'express';
-import { rateLimit } from 'express-rate-limit';
+import { Response, Request } from 'express';
+import { ipKeyGenerator, rateLimit } from 'express-rate-limit';
 import { StatusCodes } from 'http-status-codes';
+
+const ipv6Subnet = 56;
 
 export function rateLimitConfig() {
   return rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
-    limit: 100,
+    limit: 1000,
     standardHeaders: 'draft-8',
     legacyHeaders: false,
-    ipv6Subnet: 56,
-    statusCode: StatusCodes.TOO_MANY_REQUESTS,
-    message: '너무 많은 요청이 발생하였습니다. 잠시 후 다시 시도해주세요.',
-    keyGenerator: (req: Request) => req.ip || (req.ips[0] as string),
+    ipv6Subnet: ipv6Subnet,
+    validate: {
+      ipv6SubnetOrKeyGenerator: false,
+    },
+    handler: (_: Request, res: Response) => {
+      return res.status(StatusCodes.TOO_MANY_REQUESTS).json({
+        message: '너무 많은 요청이 발생하였습니다. 잠시 후 다시 시도해주세요.',
+      });
+    },
+    keyGenerator: (req: Request): string => {
+      if (req.ips[0]) {
+        return ipKeyGenerator(req.ips[0] as string, ipv6Subnet);
+      }
+
+      return ipKeyGenerator(req.ip as string, ipv6Subnet);
+    },
     skipSuccessfulRequests: false,
     skipFailedRequests: false,
   });
